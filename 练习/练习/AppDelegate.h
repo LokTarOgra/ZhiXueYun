49591ba0723eb0b4ec2890f01a258f8225c1febf
guanxindegithub <tarena160102@wangqian-2.local>
2016-07-12 20:44:35 +0800

    实验

---
+++ 练习/练习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  练习
+//
+//  Created by tarena160102 on 16/7/11.
+//  Copyright © 2016年 tarena160102. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 练习/练习/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  练习
+//
+//  Created by tarena160102 on 16/7/11.
+//  Copyright © 2016年 tarena160102. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
